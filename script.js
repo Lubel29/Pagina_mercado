9934e37c37706cdbfdbb19c8190ffee0eb4326a9
Lucia Belmonte <Lucia.belmonte@hotmail.com.ar>
2024-04-28 19:14:15 -0300

    28-4

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-
 /*bienvenida*/
-let bienvenida = prompt("Hola, bienvenido a MarketPrime. ¿Cual es tu nombre?");
-alert(`Hola ${bienvenida}, bienvenido a MarketPrime. Espero disfrutes de nuestros precios bajos`);
+// let bienvenida = prompt("Hola, bienvenido a MarketPrime. ¿Cual es tu nombre?");
+// alert(`Hola ${bienvenida}, bienvenido a MarketPrime. Espero disfrutes de nuestros precios bajos`);
 
-/*Prompt deseas comprar*/
-let seleccion = prompt("¿Deseas comprar?");
+// /*Prompt deseas comprar*/
+// let seleccion = prompt("¿Deseas comprar?");
 
-while (seleccion != "si" && seleccion != "no") {
-    alert("Por favor, ingresa 'si' o 'no' en minusculas");
-    seleccion = prompt("¿Deseas comprar?");
-}
-if (seleccion == "si") {
-    alert("Genial! compras mayores a $10.000 envío gratis");
-} else if (seleccion == "no") {
-    alert(`Te invito a navegar por nuestra pagina, ${bienvenida}, para comprar seleccionar un producto del carrito.`);
-};
+// while (seleccion != "si" && seleccion != "no") {
+//     alert("Por favor, ingresa 'si' o 'no' en minusculas");
+//     seleccion = prompt("¿Deseas comprar?");
+// }
+// if (seleccion == "si") {
+//     alert("Genial! al generar el codigo de descuento, tenés un 10% de descuento en tu compra");
+// } else if (seleccion == "no") {
+//     alert(`Te invito a navegar por nuestra pagina, ${bienvenida}, para comprar seleccionar un producto del carrito.`);
+// };
 
-/*carrito*/
-let carrito = [];
+/*---------------------------------------*/
+
+
+
+
+
+/*Productos*/
 let productos = [
-    { nombre: "Pan de salvado", precio: 1800 },
-    { nombre: "Manzana x kg", precio: 1500 },
-    { nombre: "Naranja x kg", precio: 1800 },
-    { nombre: "Arandanos x gr", precio: 2000 },
-    { nombre: "jugo de frutas", precio: 2500 },
-    { nombre: "Granola x kg", precio: 0 },
-    { nombre: "banana x kg", precio: 1600 },
-    { nombre: "Frutilla x kg", precio: 2600 },
+    { nombre: "Pan de salvado", precio: 1800, stock: 50 },
+    { nombre: "Manzana x kg", precio: 1500, stock: 80 },
+    { nombre: "Naranja x kg", precio: 1800, stock: 100 },
+    { nombre: "Arandanos x gr", precio: 2000, stock: 8 },
+    { nombre: "jugo de frutas", precio: 2500, stock: 25 },
+    { nombre: "Granola x kg", precio: 2800, stock: 0 },
+    { nombre: "banana x kg", precio: 1600, stock: 50 },
+    { nombre: "Frutilla x kg", precio: 2600, stock: 70 },
 ];
+
+
+/*---------------------------------------*/
 
 /*Recorrido del carrito de compras*/
 
@@ -36,19 +43,131 @@ for (let i = 0; i < productos.length; i++) {
 }
 
 
-/*Stock*/
-function Stock() {
-    alert("Por el momento no hay stock de " + productos[5].nombre);
+/*---------------------------------------*/
+
+
+
+/*Funcion Calcular total de productos seleccionados*/
+
+let productoSeleccionado = [productos[1], productos[3], productos[5]];
+let totalCarrito = calcularTotalCarrito(productoSeleccionado);
+
+function calcularTotalCarrito(productoSeleccionado) {
+    let total = 0;
+    for (let producto of productoSeleccionado) {
+        total += producto.precio;
+    }
+    return total;
+};
+
+console.log(`El total de tu carrito es: $${totalCarrito}`);
+
+
+/*---------------------------------------*/
+
+
+
+/*Agregar productos nuevos a la lista de productos en venta */
+
+function AgregarNuevoProducto(producto) {
+    productos.push(producto);
+    console.log(`${producto.nombre} ha sido agregado`)
 }
 
-let productoSinStock = new Stock();
+let productos_nuevo = [
+    { nombre: 'Leche', precio: 2100, stock: 150 },
+    { nombre: 'Arroz', precio: 1800, stock: 120 },
+    { nombre: 'Huevos', precio: 1800, stock: 100 },
+];
+
+productos_nuevo.forEach(producto => AgregarNuevoProducto(producto));
+
+console.log(`Cantidad de productos actualmente: ${productos.length}`);
+
+
+
+
+/*---------------------------------------*/
+
+
+
+
+/* Función para buscar productos por nombre*/
+function buscarProductoPorNombre(nombre) {
+    return productos.filter(producto => producto.nombre.toLowerCase().includes(nombre.toLowerCase()));
+}
+
+let resultadoBusqueda = buscarProductoPorNombre('Pan de salvado');
+console.log("Resultados de la búsqueda:" , resultadoBusqueda);
+
+
+
+/*---------------------------------------*/
+
+
+
+/*Consultar si hay productos sin stock*/
+
+const productosSinStock = productos.map(producto => producto.stock === 0);
+console.log("Productos sin stock:", productosSinStock);
+
+/*Me devolverá TRUE el que el stock sea 0*/
+
+
+
+
+
+/*---------------------------------------*/
+
+
+
+/*Funcion Descuento*/
+
+let porcentajeDescuento = 0.10 * 100;
+let codigoDescuento = generarCodigoDescuento();
+
+function generarCodigoDescuento() {
+    let codigo = Math.floor(Math.random() * 10000);
+    return codigo;
+};
+
+console.log("Porcentaje de descuento:", porcentajeDescuento + "%");
+console.log("Código de descuento generado: MARKET", codigoDescuento);
+
+
+// }   /*HACER FUNCION PARA APLICAR EL DESCUENTO*/
+
+
+
+/*---------------------------------------*/
+
+
+
+
+
+
+
+
+
 
 /*Contacto*/
 
-const formContact = document.getElementById('formContact');
+let nombre, email, telefono, mensaje;
 
-formContact.addEventListener('submit', (event) => {
-    event.preventDefault(); 
-    alert('Mail enviado correctamente');
-    formContact.reset();
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('formContact');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        let nombre = document.getElementById('name').value;
+        let email = document.getElementById('email').value;
+        let telefono = document.getElementById('phone').value;
+        let mensaje = document.getElementById('message').value;
+
+        console.log("Enviando datos al servidor:", nombre, email, telefono, mensaje);
+
+        document.getElementById('respuesta-envio').innerHTML = "<strong>Gracias por contactarnos, " + nombre + "!</strong><br>Hemos recibido tu mensaje";
+
+    });
 });
