a9f52134553b0106b93d719faefb9f6528bce7f9
Lucia Belmonte <Lucia.belmonte@hotmail.com.ar>
2024-04-29 20:14:49 -0300

    29-4

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 /*bienvenida*/
-// let bienvenida = prompt("Hola, bienvenido a MarketPrime. ¿Cual es tu nombre?");
-// alert(`Hola ${bienvenida}, bienvenido a MarketPrime. Espero disfrutes de nuestros precios bajos`);
+let bienvenida = prompt("Hola, bienvenido a MarketPrime. ¿Cual es tu nombre?");
+alert(`Hola ${bienvenida}, bienvenido a MarketPrime. Espero disfrutes de nuestros precios bajos`);
 
-// /*Prompt deseas comprar*/
-// let seleccion = prompt("¿Deseas comprar?");
+/*Prompt deseas comprar*/
+let seleccion = prompt("¿Deseas comprar?");
 
-// while (seleccion != "si" && seleccion != "no") {
-//     alert("Por favor, ingresa 'si' o 'no' en minusculas");
-//     seleccion = prompt("¿Deseas comprar?");
-// }
-// if (seleccion == "si") {
-//     alert("Genial! al generar el codigo de descuento, tenés un 10% de descuento en tu compra");
-// } else if (seleccion == "no") {
-//     alert(`Te invito a navegar por nuestra pagina, ${bienvenida}, para comprar seleccionar un producto del carrito.`);
-// };
+while (seleccion != "si" && seleccion != "no") {
+    alert("Por favor, ingresa 'si' o 'no' en minusculas");
+    seleccion = prompt("¿Deseas comprar?");
+}
+if (seleccion == "si") {
+    alert("Genial! al generar el codigo de descuento, tenés un 10% de descuento en tu compra");
+} else if (seleccion == "no") {
+    alert(`Te invito a navegar por nuestra pagina, ${bienvenida}, para comprar seleccionar un producto del carrito.`);
+};
 
 /*---------------------------------------*/
 
